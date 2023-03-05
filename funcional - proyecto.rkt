f56adb2dc6fef7a850f114de8776e6d58a25e952
jpesquetini <85376273+jpesquetini@users.noreply.github.com>
2023-03-05 01:51:59 -0600

    Funcion candidatos del algoritmo goloso

--- funcional - proyecto.rkt
+++ funcional - proyecto.rkt
@@ -1,13 +1,13 @@
 #lang racket
 
-(define tablero '((11 12 13 14 15 16 17 18)
-                  (21 22 23 24 25 26 27 28)
-                  (31 32 33 34 35 36 37 38)
-                  (41 42 43 44 45 46 47 48)
-                  (51 52 53 54 55 56 57 58)
-                  (61 62 63 64 65 66 67 68)
-                  (71 72 73 74 75 76 77 78)
-                  (81 82 83 84 85 86 87 88)))
+(define tablero '((1 1 2 0 0 2 1 0)
+                  (0 2 0 0 0 0 2 0)
+                  (0 1 0 0 0 0 0 0)
+                  (0 2 0 0 0 0 0 0)
+                  (0 1 0 0 0 0 0 0)
+                  (0 2 0 0 0 0 0 0)
+                  (0 1 0 0 0 0 0 0)
+                  (0 1 0 0 0 0 0 0)))
 
 (define (indice lista fila columna)
   (cond ((or (null? lista) (< fila 1) (< columna 1) (> fila (len lista)) (> columna (len (car lista))))
@@ -40,4 +40,26 @@
          (+ 1 (len (cdr lista))))
         )
   )
+
+(define (candidatos lista)
+  (cond ((null? lista)
+         lista)
+        (else
+         (candidatos_aux lista '() 1 1))
+        )
+  )
+
+(define (candidatos_aux matriz candidatos fila columna)
+  (cond ((> columna (len (car matriz)))
+         candidatos)
+        ((> fila (len matriz))
+         (candidatos_aux matriz candidatos 1 (+ columna 1)))
+        ((equal? (indice matriz fila columna) 0)
+         (candidatos_aux matriz (append candidatos (list (list fila columna))) 1 (+ columna 1)))
+        (else
+         (candidatos_aux matriz candidatos (+ fila 1) columna))
+        )
+  )
+  
+  
   
